704e4d35a4e9f99260f0d5562d74b8f9aec979d6
Rajesh Keerthi <raju.kk043@gmail.com>
2024-03-21 00:17:33 +0530

    bill print page added multiple customers

--- src/app/pages/welcome/bill-print/bill-print.component.ts
+++ src/app/pages/welcome/bill-print/bill-print.component.ts
@@ -14,6 +14,8 @@ export class BillPrintComponent implements OnInit {
   day = 0;
   tablestring = '';
   printData: any[] = [];
+  collectionsData: any[] = [];
+  date = new Date();
 
   constructor(
     private mainService: MainService
@@ -22,10 +24,10 @@ export class BillPrintComponent implements OnInit {
   ngOnInit() {
     const today_date: Date = new Date();
     this.day = today_date.getDay();
-    this.todayDate = moment(today_date).format('DD/MM/YYYY');
+    this.todayDate = moment(today_date).format('DD-MM-YYYY');
     // this.todayDate = today_date.getUTCDate() + '/' + (today_date.getUTCMonth() + 1) + '/' + today_date.getUTCFullYear();
     // this.getbalance_statement();
-    this.printCustomerBills();
+    this.getCollections();
   }
 
   showModal(): void {
@@ -33,12 +35,10 @@ export class BillPrintComponent implements OnInit {
   }
 
   handleOk(): void {
-    console.log('Button ok clicked!');
     this.isVisible = false;
   }
 
   handleCancel(): void {
-    console.log('Button cancel clicked!');
     this.isVisible = false;
   }
 
@@ -50,7 +50,6 @@ export class BillPrintComponent implements OnInit {
     const requestBody = {};
     this.mainService.get_balance_statement(requestBody).subscribe(
       (data: any) => {
-        console.log('getbalance_statement data: ', data);
         if (data.table) {
           this.tablestring = data.table;
         }
@@ -64,7 +63,6 @@ export class BillPrintComponent implements OnInit {
   getCollectionsByCustomerId(id: string) {
     this.mainService.getCollectionsByCustomerId(id).subscribe(
       (data: any) => {
-        console.log('getCollectionsByCustomerId data: ', data);
         let latestCollections: any = [];
         if (data && data.length > 0) {
           latestCollections.push(data[0]);
@@ -75,7 +73,6 @@ export class BillPrintComponent implements OnInit {
             p['collections'] = latestCollections;
           }
         });
-        console.log('print data === ', this.printData);
       },
       err => {
         console.log('get customers err ', err);
@@ -86,19 +83,16 @@ export class BillPrintComponent implements OnInit {
 
   printCustomerBills() {
     const today_date = new Date();
-    const new1 = moment(today_date).format('YYYY-MM-DD')
-    console.log('new1: ', new1);
+    const new1 = moment(this.date).format('YYYY-MM-DD')
     const requestBody = {
       'skip': 0,
       'limit': 1000,
       'bill_date': new1
     };
-    console.log('requestBody: ', requestBody);
     this.mainService.spinning.emit(true);
     this.mainService.printCustomerBills(requestBody).subscribe(
       (data: any) => {
         let customer_bills;
-        console.log('customer_bills: ', customer_bills);
         data.forEach((c: any) => {
           c['balance'] = c['collected_amount'] - c['last_amount_updated'];
           c['bill_amount'] = 0;
@@ -106,23 +100,45 @@ export class BillPrintComponent implements OnInit {
               c['bill_amount'] = c['bill_amount'] + b['total_amount']
           })
           c['total_bill'] = c['bill_amount'] - c['balance'];
+          c['collections'] = [];
+          this.collectionsData.forEach(col => {
+            if (c['cusomer_id'] === col['customer_id']) {
+              if (c['collections'].length < 2) {
+                c['collections'].push(col);
+              }
+            }
+          });
         });
         customer_bills = data;
-        console.log('after customer_bills: ', customer_bills);
         if (customer_bills) {
           this.printData = customer_bills;
         }
         this.mainService.spinning.emit(false);
-        // this.getCollectionsByCustomerId(customer_bills[0].cusomer_id);
-        // setTimeout(() => {
-        //   console.log('getCollectionsByCustomerId collection: ', collection);
-        // }, 1000);
       },
       err => {
         console.log('get customers err ', err);
         this.mainService.spinning.emit(false);
       }
     );
+  }
+
+  getCollections() {
+    const requestBody = {};
+    this.mainService.getCollections(requestBody).subscribe(
+      (data: any) => {
+        const collections = data.data;
+        this.collectionsData = collections;
+        this.printCustomerBills();
+      },
+      err => {
+        console.log('get customers err ', err);
+        this.collectionsData = [];
+      }
+    );
+  }
+
+  onChange(result: any): void {
+    this.printCustomerBills();
   }
 
 }
