452a360e264d27fe15830fd50bbc8e9f1b283ff7
vishwaNFC <158574967+vishwaNFC@users.noreply.github.com>
2024-05-16 14:30:36 +0530

    print records will come with with give maxcount

--- src/app/pages/welcome/bill-print/bill-print.component.ts
+++ src/app/pages/welcome/bill-print/bill-print.component.ts
@@ -16,10 +16,12 @@ export class BillPrintComponent implements OnInit {
   printData: any[] = [];
   collectionsData: any[] = [];
   date = new Date();
-
+  maxCount = 2
+  breakCount: number = 9; // if u want max 3 set 2 , if u want 10 recourd set 9
+  finalArray: any[] = [];
   constructor(
     private mainService: MainService
-  ) {}
+  ) { }
 
   ngOnInit() {
     const today_date: Date = new Date();
@@ -67,7 +69,7 @@ export class BillPrintComponent implements OnInit {
         if (data && data.length > 0) {
           latestCollections.push(data[0]);
           latestCollections.push(data[1]);
-        } 
+        }
         this.printData.forEach(p => {
           if (p.cusomer_id === id) {
             p['collections'] = latestCollections;
@@ -80,7 +82,67 @@ export class BillPrintComponent implements OnInit {
       }
     );
   }
+  onReturnDesiredCount(count: number, list: any[]) {
+    let filterArray = [];
+    for (let item of list) {
 
+    }
+  }
+  returnSum(customerData: any[]) {
+    return customerData.reduce((count, item) => {
+      return item.total_amount + count;
+    }, 0)
+  }
+  maxRecordsCount(customerData: any[]) {
+    let maxCountreached = true
+    let filteredArray: any[] = [];
+    customerData.forEach((dataObject, index) => {
+      if (dataObject.records.length > this.breakCount+1) {
+        let firstPart = dataObject.records.slice(0, this.breakCount )
+        // console.log('firstPart: ', firstPart);
+        let secondPart = dataObject.records.slice(this.breakCount)
+        // console.log('secondPart: ', secondPart);
+        let firstData = { ...dataObject }
+        firstData.records = firstPart;
+        let secondData = { ...dataObject }
+        secondData.records = secondPart;
+        secondData.total_amount = this.returnSum(secondData.records);
+        firstData.total_amount = this.returnSum(firstData.records);
+        // firstData['collections'] = await collectionsController.getRecentCollections(firstData.customer_id);
+        // firstData['collections'] = [];
+        // secondData['collections'] = [];
+        filteredArray.push(firstData);
+        filteredArray.push(secondData);
+      } else {
+        dataObject['total_amount'] = this.returnSum(dataObject.records);
+        // dataObject['collections'] = [];
+        // dataObject['collections'] = await collectionsController.getRecentCollections(dataObject.customer_id);
+        filteredArray.push(dataObject)
+      }
+
+    })
+    this.finalArray = filteredArray
+    // if (index + 1 == customerData.length) {
+    let hasCollectionExceedingLimit = filteredArray.some((item) => item.records.length > this.breakCount+1);
+    console.log('hasCollectionExceedingLimit', hasCollectionExceedingLimit)
+    if (hasCollectionExceedingLimit) {
+      this.maxRecordsCount(this.finalArray)
+    }else{
+      
+      console.log('filteredArray >>>>>', filteredArray)
+
+    }
+    // if (hasCollectionExceedingLimit) {
+    //   this.maxRecordsCount(filteredArray)
+    // } else {
+    //   console.log(filteredArray)
+    // }
+
+    // }
+
+
+    // console.log('filteredArray: ', filteredArray);
+  }
   printCustomerBills() {
     const today_date = new Date();
     this.day = this.date.getDay();
@@ -93,8 +155,12 @@ export class BillPrintComponent implements OnInit {
     // this.mainService.spinning.emit(true);
     this.mainService.printCustomerBills(requestBody).subscribe(
       (data: any) => {
+        let finalArray = []
         if (data && data.length > 0) {
           this.printData = data;
+          this.maxRecordsCount(data)
+          console.log('this.printData', this.printData)
+
         } else {
           this.printData = [];
         }
